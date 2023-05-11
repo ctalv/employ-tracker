7cf23d368a78830e69e48fef79eb154edeeba97d
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-11 12:30:02 -0500

    added base seed.sql file, no data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,9 @@
 -- pre-populate database (making the development of individual features much easier) --
+INSERT INTO department (name)
+VALUES ();
+
+INSERT INTO role (title, salary, department_id)
+VALUES ();
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ();
