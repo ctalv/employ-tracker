6bbd13f6c20ae65546735544a5db336161ba48e2
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-13 12:04:04 -0500

    questions pseudocode

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,1 +1,25 @@
 // questions to be asked in cli
+// view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+let viewDepartments 
+let viewRoles
+let viewEmployees
+let addDepartment
+let addRole
+let addEmployee
+let updateEmployeeRole
+
+/* BONUS 
+
+let updateEmployeeManager
+let viewEmployeesByManager
+let viewEmployeesByDepartment
+let deleteDepartment
+let deleteRole
+let deleteEmployee
+let viewDepartmentBudge
+
+*/
+const questions = [
+    
+];
+
