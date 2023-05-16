9045e6ec3d8898ebcaf4b0d1871c65dba1f2a6d2
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-15 19:51:36 -0500

    added seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,15 @@
--- pre-populate database (making the development of individual features much easier) --
 INSERT INTO department (name)
 VALUES ('Sales'),
        ('Engineering');
 
-INSERT INTO role (title, salary, department_id)
-VALUES ();
+INSERT INTO position (title, salary, department_id)
+VALUES ('Sales Lead', 100000, 1),
+       ('Salesperson', 80000, 1),
+       ('Lead Engineer', 150000, 2),
+       ('Software Engineer', 120000, 2);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ();
+VALUES ('John','Doe', 1, null),
+       ('Mike','Chan', 2, 1),
+       ('Ashley','Rodriguez', 3, null),
+       ('Kevin','Tupik', 4, 3);  
