c6a9e1adc8efa68144e7b7362f8488a58500672d
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-15 19:01:56 -0500

    added action

--- scripts/cli.js
+++ scripts/cli.js
@@ -18,6 +18,37 @@ const connection = mysql.createConnection({
 // init
 // inquirer
 
+// view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+let viewDepartments = 'department' // sql select
+let viewRoles // sql select
+let viewEmployees // sql select
+let addDepartment // sql insert into
+let addRole // sql insert into 
+let addEmployee // sql insert into
+let updateEmployeeRole // sql update?
+
+/* BONUS 
+
+let updateEmployeeManager
+let viewEmployeesByManager
+let viewEmployeesByDepartment
+let deleteDepartment
+let deleteRole
+let deleteEmployee
+let viewDepartmentBudge
+
+*/
+const variables = 
+ [
+    viewDepartments,
+    viewRoles,
+    viewEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    updateEmployeeRole
+]
+
 class CLI {
     constructor() {
 
@@ -27,7 +58,18 @@ class CLI {
             .prompt(questions)
             .then((answers) => {
                 // where is answer in array of choices
-                connection.promise().query("SELECT * FROM department")
+                console.log(answers.whatToDo)
+                console.log(questions[0].choices)
+
+                let index = questions[0].choices.indexOf(answers.whatToDo[0]);
+
+                console.log(variables[index])
+
+                let action = variables[index]
+                
+                
+
+                connection.promise().query(`SELECT * FROM ${action}`)
                     .then(([rows, fields]) => {
                         console.table(rows);
                     })
--- scripts/sql.js
+++ scripts/sql.js
@@ -10,17 +10,17 @@ const connection = mysql.createConnection({
   });
 
 
-  async function main() {
-    // get the client
-    const mysql = require('mysql2/promise');
-    // create the connection
-    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'test'});
-    // query database
-    const [rows, fields] = await connection.execute('SELECT * FROM `table` WHERE `name` = ? AND `age` > ?', ['Morty', 14]);
-  }
+  // async function main() {
+  //   // get the client
+  //   const mysql = require('mysql2/promise');
+  //   // create the connection
+  //   const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'test'});
+  //   // query database
+  //   const [rows, fields] = await connection.execute('SELECT * FROM `table` WHERE `name` = ? AND `age` > ?', ['Morty', 14]);
+  // }
 
 // view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-let viewDepartments // sql select
+let viewDepartments = 'department' // sql select
 let viewRoles // sql select
 let viewEmployees // sql select
 let addDepartment // sql insert into
@@ -53,7 +53,7 @@ const variables =
 function choices(answers) {
   let index = questions[0].choices.indexOf(answers);
 
-  variables[index]
+  console.log(variables[index])
 
   
 }
